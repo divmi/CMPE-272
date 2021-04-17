ee1a7f54ed6d9eac206066c5feaeeffcdc89e1f0
divya mittal <divyamittal@divyas-MacBook-Pro.local>
2021-04-17 00:18:06 -0700

    test

--- DataBaseConnection.php
+++ DataBaseConnection.php
@@ -1,10 +1,11 @@
 <?php
 function OpenCon()
 {
-    $dbhost = "localhost";
+    $dbhost = "127.0.0.1";
     $dbuser = "root";
     $dbpass = "Divmi@12345";
     $db = "TravelCompany";
+    echo 'dbhost';
     $conn = new mysqli($dbhost, $dbuser, $dbpass, $db) or die("Connect failed: %s\n" . $conn->error);
 
     return $conn;
