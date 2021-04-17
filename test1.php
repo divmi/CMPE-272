9081ed47efaf48b9bbd1b4ef2e8cbbe3eac3faac
divya mittal <divyamittal@divyas-MacBook-Pro.local>
2021-04-16 23:17:52 -0700

    test file

--- test1.php
+++ test1.php
@@ -3,12 +3,9 @@
 include 'DataBaseConnection.php'; // includes the connection.php file to connect to the database
 $conn = OpenCon();
 // query to database and prepares and executes
-if (isset($_POST['search'])) {
-    $search_value = $_POST['searchValue'];
-    $query = "SELECT * FROM UserRegisterationTable WHERE (`FirstName` LIKE '%$search_value%') OR (`LastName` LIKE '%$search_value%')  OR (`Email` LIKE '%$search_value%')  OR (`HomePhone` LIKE '%$search_value%')  OR (`CellPhone` LIKE '%$search_value%');";
-    $result = mysqli_query($conn, $query);
+$query = "SELECT * FROM UserRegisterationTable";
+$result = mysqli_query($conn, $query);
 
-} else {
-    $query = "SELECT * FROM UserRegisterationTable";
-    $result = mysqli_query($conn, $query);
+while ($data = mysqli_fetch_array($result)) {
+    echo $data["FirstName"];
 }
