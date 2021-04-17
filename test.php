c957474d1da55feef342029599b781c450b17f57
divya mittal <divyamittal@divyas-MacBook-Pro.local>
2021-04-17 10:10:36 -0700

    test

--- test.php
+++ test.php
@@ -8,6 +8,6 @@ $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
 echo "<h2>TODO</h2><ol>";
 foreach ($db->query("SELECT FirstName FROM $table") as $row) {
     echo "<li>" . $row['FirstName'] . "</li>";
-    echo "</ol>";}
-print("<div><a href='/usersection.html'>Click here to go back to user page</a></div>");
+}
+echo "</ol>";
 ?>
