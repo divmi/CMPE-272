aca710a15faab65ff3da1b1421e66aa7203f51fd
divya mittal <divyamittal@divyas-MacBook-Pro.local>
2021-04-17 09:59:56 -0700

    Test

--- test1.php
+++ test1.php
@@ -1,10 +1,16 @@
 <?php
-
-$conn = mysqli_connect('localhost', 'root', 'Divmi@12345', 'TravelCompany');
-// query to database and prepares and executes
-$query = "SELECT * FROM UserRegisterationTable";
-$result = mysqli_query($conn, $query);
-
-while ($data = mysqli_fetch_array($result)) {
-    echo $data["FirstName"];
+$user = "root";
+$password = "Divmi@12345";
+$database = "TravelCompany";
+$table = "UserRegisterationTable";
+try {
+    $db = new PDO("mysql:host=localhost;dbname=$database", $user, $password);
+    echo "<h2>TODO</h2><ol>";
+    foreach ($db->query("SELECT FirstName FROM $table") as $row) {
+        echo "<li>" . $row['FirstName'] . "</li>";
+    }
+    echo "</ol>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
 }
